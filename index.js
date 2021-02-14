15dec2caaebe1dc48126451cf122d9db029f7a99
V. Aret <vassilis@aretakis.eu>
2021-02-14 13:33:45 +0100

    add a first failed try

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const promClient = require('prom-client');
 const Registry = promClient.Registry;
 
-const logger = reqlib('/lib/logger.js').module('Prometheus')
+const logger = require('../../lib/logger.js').module('Prometheus')
 
 const DEFAULT_PROMETHEUS_METRICS_PATH = '/metrics';
 
 function PromClient (zwave) {
-  this.zwave = zwave
+  if (!(this instanceof PromClient)) {
+    logger.info('This is the PromClient init')
+    logger.info(`Zwave length is ${zwave.length()}`)
+    this.zwave = zwave
+    PromClient.start(zwave)
+  }
+  logger.info('Next step of PromClient')
+
 }
 
 PromClient.prototype.start = async function () {
-
+    logger.info('Event caller')
     if (this.zwave) {
         this.zwave.on('valueChanged', onValueChanged.bind(this))
         this.zwave.on('nodeRemoved',  onNodeRemoved.bind(this))
@@ -25,3 +32,5 @@ function onNodeRemoved (node) {
 function onValueChanged (valueId, node, changed) {
   logger.info(`Value is ${changed}`)
 }
+
+module.exports = PromClient
