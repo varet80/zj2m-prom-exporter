d63acaf3af4efb6a6e5a6607b4f2da8a0457f78f
V. Aret <vassilis@aretakis.eu>
2021-02-15 17:11:44 +0100

    add the variable declaration

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const logger = require('../../lib/logger.js').module('Prometheus')
 
 const DEFAULT_PROMETHEUS_METRICS_PATH = '/metrics';
 
-function PromClient () {
+function PromClient (zwave) {
   if (!(this instanceof PromClient)) {
     logger.info('This is the PromClient init')
-    d = new PromClient()
+    d = new PromClient(zwave)
     d.start()
   }
   logger.info('Next step of PromClient')
@@ -18,6 +18,7 @@ function PromClient () {
 PromClient.prototype.start = async function () {
     logger.info('Event caller')
     if (this.zwave) {
+        logger.info('this.zwave exists')
         this.zwave.on('valueChanged', onValueChanged.bind(this))
         this.zwave.on('nodeRemoved',  onNodeRemoved.bind(this))
     }
