ca8248d1ac84976089b0ce0e553cfffb2948e724
V. Aretakis <v.aretakis@elsevier.com>
2021-02-16 15:24:27 +0100

    separate to new file

---
+++ HttpServer.js
@@ -0,0 +1,22 @@
+const { httpMetricPath, logger, httpPort, httpAddr } = require('./index')
+const fastify = require('fastify')()
+// Http Server to return metrics
+function HttpServer (customRegistry) {
+  // Declare a route
+  fastify.get(httpMetricPath, async (request) => {
+    logger.info(`Metrics query from ${request.ip}`)
+    return customRegistry.metrics()
+  });
+
+  // Run the server!
+  const start = async () => {
+    try {
+      await fastify.listen(httpPort, httpAddr)
+    } catch (err) {
+      fastify.log.error(err)
+      process.exit(1)
+    }
+  };
+  start()
+}
+exports.HttpServer = HttpServer
