45c22305cf23449e5b4b888f408812eb78927556
V. Aretakis <v.aretakis@elsevier.com>
2021-02-16 15:07:26 +0100

    lint

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const promCli = require('prom-client');
+const promCli = require('prom-client')
 const logger = require('../../lib/logger.js').module('Prometheus')
 const fastify = require('fastify')()
 
@@ -13,14 +13,13 @@ let instance = null // the singleton instance
  * Registry and Gauge settings for Prometheus
  */
 const PromCliRegistry = promCli.Registry
-let customRegistry = new PromCliRegistry();
+const customRegistry = new PromCliRegistry()
 const gauge = new promCli.Gauge({
   registers: [customRegistry],
   name: 'zj2m',
   help: 'zwavejs2mqtt gauges from metrics',
-  labelNames: ['nodeId', 'location', 'name', 'commandClass', 'property', 'propertyKey', 'label', 'type', 'endpoint', 'id'],
-});
-
+  labelNames: ['nodeId', 'location', 'name', 'commandClass', 'property', 'propertyKey', 'label', 'type', 'endpoint', 'id']
+})
 
 // Http Server to return metrics
 function HttpServer (customRegistry) {
@@ -29,7 +28,7 @@ function HttpServer (customRegistry) {
     logger.info(`Metrics query from ${request.ip}`)
     return customRegistry.metrics()
   })
-  
+
   // Run the server!
   const start = async () => {
     try {
@@ -46,31 +45,29 @@ function HttpServer (customRegistry) {
  * Function to initiate the Client (plugin)
  **/
 function PromClient (zwave) {
-  if(instance) {
+  if (instance) {
     instance.destroy()
   }
   this.zwave = zwave
   if (!(this instanceof PromClient)) {
-
     // start http server
     HttpServer(customRegistry)
 
     d = new PromClient(zwave)
     d.start()
- }
-  
-  instance = this
+  }
 
+  instance = this
 }
 
 PromClient.prototype.start = async function () {
-    logger.info('Event caller')
-    if (this.zwave) {
-        this.zwave.on('valueChanged', onValueChanged.bind(this))
-        this.zwave.on('nodeRemoved',  onNodeRemoved.bind(this))
-    }
-    // this is async but doesn't need to be awaited
-    //this.zwave.connect()
+  logger.info('Event caller')
+  if (this.zwave) {
+    this.zwave.on('valueChanged', onValueChanged.bind(this))
+    this.zwave.on('nodeRemoved', onNodeRemoved.bind(this))
+  }
+  // this is async but doesn't need to be awaited
+  // this.zwave.connect()
 }
 
 // Implements the Payload for gauge, and registers/upgrade gauge
@@ -83,20 +80,20 @@ function gaugePayload (payload) {
       return
   }
 
-  let metricValue = 0;
-  switch(typeof payload.value) {
-  case "number":
-    metricValue = payload.value
-    break;
-  case "boolean":
-    if (payload.value) {
-      metricValue = 1
-    }
-  default:
-    return;
+  let metricValue = 0
+  switch (typeof payload.value) {
+    case 'number':
+      metricValue = payload.value
+      break
+    case 'boolean':
+      if (payload.value) {
+        metricValue = 1
+      }
+    default:
+      return
   }
   logger.info(`Adding value to metric ${payload.id}`)
-  let gaugeLabels = {
+  const gaugeLabels = {
     nodeId: payload.nodeId,
     name: payload.nodeName,
     location: payload.nodeLocation,
@@ -108,17 +105,15 @@ function gaugePayload (payload) {
     endpoint: payload.endpoint,
     id: payload.id
   }
-  //set gauge
+  // set gauge
   gauge.set(gaugeLabels, metricValue)
   logger.debug(`Registered ${metricValue} under ${payload.id}`)
-  return
 }
 
 // TODO: Placeholder for removal
 function onNodeRemoved (node) {
-    return
-}
 
+}
 
 /**
  * Value changes calls for change
